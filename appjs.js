aba6554c2af8a93056c2a862915285bab0cb3423
ammar-abuamra <3mmarabuamra@gmail.com>
2020-12-23 15:20:48 +0200

    try java

---
+++ appjs.js
@@ -0,0 +1,8 @@
+
+
+
+var ask = prompt('i wolud love to insert your name')
+
+var ageask = confirm('this is a medical site maybe it will have +18 photo would you love to continue ?')
+
+alert('Hello  ' + ask + '  Hope you have a good day')
