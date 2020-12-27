1689dbcdff887aa26ce1c46ba304c35f503d2b69
ammar-abuamra <3mmarabuamra@gmail.com>
2020-12-27 14:09:25 +0200

    photoloop

--- appjs.js
+++ appjs.js
@@ -1,8 +1,22 @@
 
 
 
-var ask = prompt('i wolud love to insert your name')
+var ask = prompt(' wolud you love to insert your name')
 
-var ageask = confirm('this is a medical site maybe it will have +18 photo would you love to continue ?')
+
+while (ask == "") {
+    ask = prompt('يارجل دخل اسمك')
+}
 
 alert('Hello  ' + ask + '  Hope you have a good day')
+
+
+
+
+
+var photoloop = prompt('i will show you some magic but a number from 1 to 5')
+for ( var i=0; i<=photoloop; i++) {  document.write("<img src='https://scontent.famm3-3.fna.fbcdn.net/v/t1.0-9/67430996_736155613487640_2707608968934457344_n.png?_nc_cat=107&ccb=2&_nc_sid=730e14&_nc_eui2=AeHBcbG2L1kwstXx58auitNdWC8LYOaFwR5YLwtg5oXBHh9Bw8WTKdlz0Cp3rBojhXaai81mPNrPpgPijy-8h6tC&_nc_ohc=5oRtGIQ_JqkAX-b7Zt9&_nc_ht=scontent.famm3-3.fna&oh=4dae89fb2bf91cdb941353cbbb682e2f&oe=6008387C'>") }
+
+
+
+// document.write("<img src='https://scontent.famm3-3.fna.fbcdn.net/v/t1.0-9/67430996_736155613487640_2707608968934457344_n.png?_nc_cat=107&ccb=2&_nc_sid=730e14&_nc_eui2=AeHBcbG2L1kwstXx58auitNdWC8LYOaFwR5YLwtg5oXBHh9Bw8WTKdlz0Cp3rBojhXaai81mPNrPpgPijy-8h6tC&_nc_ohc=5oRtGIQ_JqkAX-b7Zt9&_nc_ht=scontent.famm3-3.fna&oh=4dae89fb2bf91cdb941353cbbb682e2f&oe=6008387C'>")
